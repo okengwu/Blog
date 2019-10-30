4d0c81c89ff2dc6b833045f653cef4458fb91495
okengwu <okengwuchidiebere@yahoo.com>
2019-10-30 12:27:51 -0400

    first commit

---
+++ myBlog/blogapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Post  # import the table to work on the admin 
+
+admin.site.register(Post) # this helps to register to help you work from the dashboard
+
+# Register your models here.
---
+++ myBlog/blogapp/models.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.db import models # this django modes is use to create a table on database
+from django.utils import timezone # time zone helps to identify ur geogrphical time when working on time in inputs
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE) # by convention django AUTH is use when foriegn key is there
+    title = models.CharField(max_length = 200) 
+    text = models.TextField()
+    created_date = models.DateTimeField(default = timezone.now)
+    published_date = models.DateTimeField(blank = True, null = True)
+
+# this method is use to publish 
+def publish(self):
+    self.published_date = time
+    self.save()
+
+# by convention this comes with every model table to return the toString of title in this case
+def __str__(self):
+    return self.title
+
+
+# Create your models here.
