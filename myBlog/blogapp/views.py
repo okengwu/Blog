811e805e57330e3c376c9856e2729f46ae184950
okengwu <okengwuchidiebere@yahoo.com>
2019-10-30 17:28:25 -0400

    third commit

--- myBlog/blogapp/views.py
+++ myBlog/blogapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
+
+def post_list(request):
+   # face = request.POST['www.facebook.com']
+    return render(request,'blogapp/post_list.html', {
+      #  "facebook" : face
+
+    })
+
+
 # Create your views here.
